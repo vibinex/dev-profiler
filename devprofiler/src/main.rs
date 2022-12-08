59ff7aff6acc32c1093d79ee3e16dc8b0465aa00
Tapish Rathore <tapish303@gmail.com>
2022-12-08 15:10:49 +0530

    Working on map reduce and packaging

--- devprofiler/src/main.rs
+++ devprofiler/src/main.rs
@@ -1,14 +1,11 @@
 #![allow(unused)]
 
-// use clap::Parser;
-// use anyhow::{Context, Result};
-// use std::fs::File;
-use walkdir::{WalkDir, DirEntry};
-use std::path::{Path, PathBuf};
-use std::fs;
-use time;
+use workerpool::Pool;
+use workerpool::thunk::{Thunk, ThunkWorker};
 use git2::{Commit, ObjectType, Repository};
-/// Search for a pattern in a file and display the lines that contain it.
+use std::sync::mpsc::channel;
+use std::sync::{Arc, Mutex};
+// Search for a pattern in a file and display the lines that contain it.
 // #[derive(Parser)]
 // struct Cli {
 //     /// The pattern to look for
@@ -20,116 +17,47 @@ use git2::{Commit, ObjectType, Repository};
 // fn is_git(entry: &DirEntry) -> bool {
 //     entry.file_name().to_str().map(|s| s == ".git").unwrap_or(false)
 // }
-fn is_hidden(entry: &DirEntry) -> bool {
-    entry
-         .file_name()
-         .to_str()
-         .map(|s| s.starts_with("."))
-         .unwrap_or(false)
-}
 
-fn map_metrics(commit: &Commit) {
-    let timestamp = commit.time().seconds();
-    let tm = time::at(time::Timespec::new(timestamp, 0));
-    println!("commit {}\nAuthor: {}\nDate:   {}\n\n    {}",
-             commit.id(),
-             commit.author(),
-             tm.rfc822(),
-             commit.message().unwrap_or("no commit message"));
+unsafe impl Send for Commit {}
+
+fn map_metrics(commit: &Commit) -> String {
+    commit.id().to_string()
 }
 
 fn some_commit() -> Result<i32, Box<git2::Error>> {
-    let repo = Repository::discover("/Users/tapishpersonal/Code/mentorship-website")?;
+    let repo = Repository::discover("/home/tapishr/mentorship-website")?;
         let mut revwalk = repo.revwalk()?;
         let mut count = 0;
+        let n_workers = 4;
+        let pool = Pool::<ThunkWorker<String>>::new(n_workers);
+        let (tx, rx) = channel();
         revwalk.push_head()?;
         // revwalk.set_sorting(git2::Sort::TIME)?;
         for rev in revwalk {
             let commit = repo.find_commit(rev?)?;
             let message = commit.summary_bytes().unwrap_or_else(|| commit.message_bytes());
-            // let t = commit.time();
             let author = commit.author();
+            let mut_commit = Arc::new(Mutex::new(commit));
             if author.to_string().contains("tapish") {
-                println!("{}\t{}\t{}", commit.id(), author, String::from_utf8_lossy(message));
                 count = count + 1;
+                pool.execute_to(tx.clone(), Thunk::of(move || -> String {
+                    let refcommit = mut_commit.lock().unwrap();
+                    map_metrics(&refcommit)
+                }));
             }
         }
         println!("Number of commits: {}", count);
+        let threads = rx.iter();
         Ok(0)
-    // let repo = match Repository::open("/Users/tapishpersonal/Code/mentorship-website") {
-    //     Ok(repo) => repo,
-    //     Err(e) => panic!("failed to open: {}", e),
-    // };
-    // let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
-    // obj.into_commit().map_err(|_| git2::Error::from_str("Couldn't find commit"))
 }
 
 
 fn main() {
     // let args = Cli::parse();
-    let repo_path = [Path::new("/Users/tapishpersonal/Code/mentorship-website")];
-    let aliases = [""];
-    for repo in repo_path {
-        // get all branches
-        let mut refheads = repo.to_path_buf();
-        refheads.push(".git");
-        refheads.push("refs");
-        refheads.push("heads");
-        let mut heads = Vec::new();
-        // iterate over dirs and files
-        let mut packedrefs = repo.to_path_buf();
-        packedrefs.push(".git");
-        packedrefs.push("packed-refs");
-        let mut content = fs::read_to_string(packedrefs)
-                .expect("Unable to read file");
-        println!("{}", content);
-        let walker = WalkDir::new(refheads).into_iter(); // TODO - do correct error handling here
-        for entry in walker.filter_map(|e| e.ok())
-            .filter(|e| e.file_type().is_file() && !is_hidden(e)) { // is_hidden to avoid files like .DS_Store
-            // files are read and commits stored in lists
-            let mut commit = fs::read_to_string(entry.path())
-                .expect("Unable to read file");
-            commit.pop(); // remove trailing newline
-            heads.push(commit);
-        }
-        println!("{:?}", heads);
-
-        // open some branch file and go over all commits, filter by aliases
-        // for each commit, get stats -> in worker threads
-    }
     let res = some_commit();
-    // map_metrics(obj);
-
-    // wait for all threads to finish
-    // do the reduce step for each metric (like no of years in each lang) in diff thread
-    // wait for threads to finish
-    // render jsonl
-
-    // Note: See that .expect method here? This is a shortcut function to quit that will make the program exit immediately when the value (in this case the input file) could not be read. It’s not very pretty, and in the next chapter on Nicer error reporting we will look at how to improve this.
-    // let content = std::fs::read_to_string(&args.path)
-    //     .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
-    // This is not the best implementation: It will read the whole file into memory – however large the file may be. Find a way to optimize it! (One idea might be to use a BufReader instead of read_to_string()
-    // for line in content.lines() {
-    //     if line.contains(&args.pattern) {
-    //         println!("{}", line);
-    //     }
-    // }
-
-    //Scanner
-    // let walker = WalkDir::new("/").into_iter();
-    // for entry in walker.filter_map(Result::ok).filter(|e| is_git(e)) {
-        // Also see filter_entry
-    //     println!("{}", entry.path().display());
-    // }
-
-    // Ok(())
-    // let mut f = File::open("/Users/tapishpersonal/Code/mentorship-website/.git/objects/bb/721ad95595bd8e023387e7af46aef3480c80f3")?;
-    // let metadata = f.metadata()?;
-    // for (key, value) in metadata {
-    //     println!("{}: {}", key, value);
-    // }
-    // Ok(())
 }
+
+
 fn answer() -> i64 {
     (42)
 }
