e4c5840cdf1abef350a20af28d14ced23f312a33
Tapish Rathore <tapish303@gmail.com>
2022-12-11 20:42:54 +0530

    Add travis ci and appveyor for packaging

--- devprofiler/src/main.rs
+++ devprofiler/src/main.rs
@@ -3,6 +3,15 @@ use workerpool::thunk::{Thunk, ThunkWorker};
 use git2::{Commit, Repository};
 use std::sync::mpsc::channel;
 use std::sync::Mutex;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// author name or pattern
+    author: String,
+    /// repository path
+    path: std::path::PathBuf,
+}
 
 #[derive(Clone)]
 struct CommitInfo {
@@ -25,20 +34,20 @@ fn map_metrics(cinfo: &CommitInfo) -> String {
     cinfo.clone().author
 }
 
-fn some_commit() -> Result<i32, Box<git2::Error>> {
-    let repo = Repository::discover("/home/tapishr/mentorship-website")?;
-        let mut revwalk = repo.revwalk()?;
+fn analyze_repo(arg_ref: &Cli) {
+    let repo = Repository::discover(&arg_ref.path).unwrap();
+        let mut revwalk = repo.revwalk().unwrap();
         let mut count = 0;
         let n_workers = 4;
         let pool = Pool::<ThunkWorker<String>>::new(n_workers);
         let (tx, rx) = channel();
-        revwalk.push_head()?;
+        revwalk.push_head().unwrap();
         // revwalk.set_sorting(git2::Sort::TIME)?;
         for rev in revwalk {
-            let commit = repo.find_commit(rev?)?;
+            let commit = repo.find_commit(rev.unwrap()).unwrap();
             // let message = commit.summary_bytes().unwrap_or_else(|| commit.message_bytes());
             let author = commit.author();
-            if author.to_string().contains("tapish") {
+            if author.to_string().contains(&(arg_ref.author)) {
                 count = count + 1;
                 // let ncommit = commit.clone();
                 let arc_commit = Mutex::new(CommitInfo::new(commit.clone()));
@@ -53,12 +62,12 @@ fn some_commit() -> Result<i32, Box<git2::Error>> {
         for r in rx.iter() {
             println!("rx = {:?}", r);
         }
-        Ok(0)
 }
 
 
 fn main() {
-    let _res = some_commit();
+    let args = Cli::parse();
+    analyze_repo(&args);
 }
 
 
