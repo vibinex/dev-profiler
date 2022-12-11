ed7cce8cd522c4766439bc78b53dc849c9a09712
Tapish Rathore <tapish303@gmail.com>
2022-12-11 18:17:15 +0530

    implement workerpool

--- devprofiler/src/main.rs
+++ devprofiler/src/main.rs
@@ -1,27 +1,28 @@
-#![allow(unused)]
-
 use workerpool::Pool;
 use workerpool::thunk::{Thunk, ThunkWorker};
-use git2::{Commit, ObjectType, Repository};
+use git2::{Commit, Repository};
 use std::sync::mpsc::channel;
-use std::sync::{Arc, Mutex};
-// Search for a pattern in a file and display the lines that contain it.
-// #[derive(Parser)]
-// struct Cli {
-//     /// The pattern to look for
-//     pattern: String,
-//     /// The path to the file to read
-//     path: std::path::PathBuf, // PathBuf is like a String but for file system paths that work cross-platform.
-// }
+use std::sync::Mutex;
 
-// fn is_git(entry: &DirEntry) -> bool {
-//     entry.file_name().to_str().map(|s| s == ".git").unwrap_or(false)
-// }
+#[derive(Clone)]
+struct CommitInfo {
+    author: String,
+    // commit_id: String,
+}
 
-unsafe impl Send for Commit {}
+impl CommitInfo {
+    fn new(commit: Commit) -> Self {
+        Self {
+            author: commit.author().to_string(),
+            // commit_id: commit.id().to_string(),
+        }
+    }
+}
 
-fn map_metrics(commit: &Commit) -> String {
-    commit.id().to_string()
+unsafe impl Send for CommitInfo {}
+
+fn map_metrics(cinfo: &CommitInfo) -> String {
+    cinfo.clone().author
 }
 
 fn some_commit() -> Result<i32, Box<git2::Error>> {
@@ -35,40 +36,43 @@ fn some_commit() -> Result<i32, Box<git2::Error>> {
         // revwalk.set_sorting(git2::Sort::TIME)?;
         for rev in revwalk {
             let commit = repo.find_commit(rev?)?;
-            let message = commit.summary_bytes().unwrap_or_else(|| commit.message_bytes());
+            // let message = commit.summary_bytes().unwrap_or_else(|| commit.message_bytes());
             let author = commit.author();
-            let mut_commit = Arc::new(Mutex::new(commit));
             if author.to_string().contains("tapish") {
                 count = count + 1;
+                // let ncommit = commit.clone();
+                let arc_commit = Mutex::new(CommitInfo::new(commit.clone()));
                 pool.execute_to(tx.clone(), Thunk::of(move || -> String {
-                    let refcommit = mut_commit.lock().unwrap();
+                    let refcommit = arc_commit.lock().unwrap();
                     map_metrics(&refcommit)
                 }));
             }
         }
+        drop(tx);
         println!("Number of commits: {}", count);
-        let threads = rx.iter();
+        for r in rx.iter() {
+            println!("rx = {:?}", r);
+        }
         Ok(0)
 }
 
 
 fn main() {
-    // let args = Cli::parse();
-    let res = some_commit();
+    let _res = some_commit();
 }
 
 
-fn answer() -> i64 {
-    (42)
-}
-#[test]
-// test all branches are there
-// test filtering of commits, if all commits visited and filtering is correct
-// test correctness of stats
-// test reduce step correctness
-// test final calculation
-// test jsonl rendering for correctness
-// test thread management? if all are finished etc
-fn check_answer_validity() {
-    assert_eq!(answer(), 42);
-}
+// fn answer() -> i64 {
+//     42
+// }
+// #[test]
+// // test all branches are there
+// // test filtering of commits, if all commits visited and filtering is correct
+// // test correctness of stats
+// // test reduce step correctness
+// // test final calculation
+// // test jsonl rendering for correctness
+// // test thread management? if all are finished etc
+// fn check_answer_validity() {
+//     assert_eq!(answer(), 42);
+// }
