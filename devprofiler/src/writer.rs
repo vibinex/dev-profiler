1c2a93c84d430c27b26c9de47781049fa20e3b39
Tapish Rathore <tapish303@gmail.com>
2023-01-01 19:34:32 +0530

    Write io errors to separate file as they are large in number sometimes

--- devprofiler/src/writer.rs
+++ devprofiler/src/writer.rs
@@ -6,7 +6,8 @@ use flate2::Compression;
 use flate2::write::GzEncoder;
 
 pub struct OutputWriter {
-    writer: GzEncoder<BufWriter<File>>
+    writer: GzEncoder<BufWriter<File>>,
+    iowriter: Option<BufWriter<File>>
 }
 
 impl OutputWriter {
@@ -14,14 +15,38 @@ impl OutputWriter {
         let file = File::create("devprofile.jsonl.gz")?;
         let bufw = BufWriter::new(file);
         let gze = GzEncoder::new(bufw, Compression::default());
-        Ok(Self{writer: gze})
+        Ok(Self{
+            writer: gze,
+            iowriter: None,
+        })
     }
 
     pub fn writeln(&mut self, line: &str) -> Result<(), Error>{
         writeln!(self.writer, "{}", line.to_string())
     }
 
+    pub fn write_io_err(&mut self, line: &str) -> Result<(), Error>{
+        if self.iowriter.is_none() {
+            match self.create_io_file_writer() {
+                Ok(io_writer) => {self.iowriter = io_writer;},
+                Err(error) => { return Err(error); },
+            }
+            
+        }
+        let writer_borrow = self.iowriter.as_mut().expect("Checked, is some");
+        writeln!(writer_borrow, "{}", line.to_string())
+    }
+
     pub fn finish(&mut self) -> Result<(), Error>{
+        if self.iowriter.is_some() {
+            let writer_borrow = self.iowriter.as_mut().expect("Checked, is some");
+            writer_borrow.flush()?;
+        }
         self.writer.try_finish()
+    }
+
+    fn create_io_file_writer(&self) -> Result<Option<BufWriter<File>>, Error>{
+        let iofile = File::create("io_errors.txt")?;
+        Ok(Some(BufWriter::new(iofile)))
     }
 }
