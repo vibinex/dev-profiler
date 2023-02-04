dc07b21ab3d8c8a58bc77d1c5f3008c108b11ef6
Tapish Rathore <tapish303@gmail.com>
2023-02-04 13:09:32 +0530

    fix curl command filepath

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ timestamp=$(date +%s)
 filename="${timestamp}-devprofile.jsonl.gz"
 mv devprofile.jsonl.gz "${filename}"
 
-curl -F "file=@/${filename}"  https://gcscruncsql-k7jns52mtq-el.a.run.app/upload
+curl -F "file=@${filename}"  https://gcscruncsql-k7jns52mtq-el.a.run.app/upload
