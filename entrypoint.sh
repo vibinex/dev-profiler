af30597fb5da9655b46ea17fed753c87166786b9
Tapish Rathore <tapish303@gmail.com>
2023-02-05 20:58:21 +0530

    Remove github action files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-devprofiler -- docker
-
-timestamp=$(date +%s)
-filename="${timestamp}-devprofile.jsonl.gz"
-mv devprofile.jsonl.gz "${filename}"
-
-curl -F "file=@${filename}"  https://gcscruncsql-k7jns52mtq-el.a.run.app/upload
